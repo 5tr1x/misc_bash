ebc35b32d4abbf0dfe4e152124a46bd107937da4
5tr1x <65570372+5tr1x@users.noreply.github.com>
2025-04-08 11:36:01 -0500

    Update create-wordlist.sh

--- create-wordlist.sh
+++ create-wordlist.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-for i in `cat $1`; do echo ${i}; echo ${i}2019; echo ${i}2020; echo ${i}2021; echo ${i}2022; echo ${i}1; echo ${i}123; done >> n
-for i in `cat n`; do echo ${i}; echo ${i}'!'; echo ${i}'@'; echo ${i}'#'; echo ${i}'$'; echo ${i}'%'; echo ${i}'^'; echo ${i}'&'; echo ${i}'*'; echo ${i}'?'; done >> s
+if [ -z $1 ]
+then
+        echo 'Usage: password-permutator <word>'
+        exit 1
+fi
+
+echo ''
+echo '[*] generating permutations.....'
+echo ''
+
+for i in `echo $1 | tr '[:upper:]' '[:lower:]'`; do echo ${i}; echo ${i}2021; echo ${i}2022; echo ${i}2023; echo ${i}2024; echo ${i}2025; echo ${i}1; echo ${i}2; echo ${i}3; echo ${i}4; echo ${i}5; echo ${i}123; echo ${i}1234; echo ${i}12345; done >> n
+for i in `cat n`; do echo ${i}; echo ${i}'!'; echo ${i}'@'; echo ${i}'#'; echo ${i}'$'; echo ${i}'%'; echo ${i}'^'; echo ${i}'&'; echo ${i}'*'; echo ${i}'-'; echo ${i}'_'; echo ${i}'='; echo ${i}'+'; echo ${i}'?'; done >> s
 hashcat s -r /usr/share/hashcat/rules/leetspeak.rule --stdout > hc
 wordlister --input s --output w1 --perm 1 --min 4 --max 48 --leet &>/dev/null
 cat hc w1 | sort -u > xx
 wordlister --input xx --output w2 --perm 1 --min 4 --max 48 --cap --up &>/dev/null
-cat w2 | sort -u > $2
+cat w2 | sort -u > permutations.txt
 rm n s hc w1 xx w2
